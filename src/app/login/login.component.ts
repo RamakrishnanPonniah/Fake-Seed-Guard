c4c0b8f4d1d3bc607c0e3725de73d5a8c8f6a38c
gowthamkannan14 <chinnagowtham14@gmail.com>
2019-06-15 00:19:41 +0530

    Design Update 2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ConversationService } from '../conversation.service';
+import { MediaMatcher } from "@angular/cdk/layout";
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ConversationService } from "../conversation.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
-export class LoginComponent implements OnInit {
-
+export class LoginComponent implements OnInit, OnDestroy {
   loginError;
-  constructor(private router:Router, private conversation:ConversationService) { }
-
-  ngOnInit() {
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private router: Router,
+    private conversation: ConversationService
+  ) {
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  login(form:NgForm){
-    if(form.value.uname=='user' && form.value.psw =='isg'){
-      sessionStorage.setItem('loggedIn', 'true');
-      this.router.navigate(['/home']);
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  ngOnInit() {}
+
+  login(form: NgForm) {
+    if (form.value.uname == "user" && form.value.psw == "isg") {
+      sessionStorage.setItem("loggedIn", "true");
+      this.router.navigate(["/home"]);
       this.conversation.setloggedInStatus(true);
-    }
-    else{
+    } else {
       this.loginError = true;
     }
   }
-
 }
